0015752f019bbda87b2bde1c8213eedb84bd2dce
gtfarng <5735512002@psu.ac.th>
2019-09-26 15:47:47 +0700

    update

--- query/index.js
+++ query/index.js
@@ -24,18 +24,48 @@ app.get('/api/DoctorListDetails',  (req, res) =>{
         const request = new sql.Request();
 
         // query to the database and get the data
-        request.execute('dbo.DN_DoctorListDetalis',  (err, DoctorListDetails) =>{
-            console.log(DoctorListDetails)
+        request.execute('dbo.DN_DoctorListDetalis',  (err, recordset) =>{
+           // console.log(recordset)
+           console.log('.GET :/api/DoctorListDetails')
             if (err) console.log(err)
 
             // send data as a response
-            res.send(DoctorListDetails);
-            sql.close()
+            res.send(recordset);
+           
 
         });
 
     });
+     sql.close()
 });
+
+
+app.post('/api/test',  (req, res)=> {
+    const hn = req.body.HN;
+   
+    sql.connect(config,  (err) =>{
+
+        if (err) console.log(err);
+        // create Request object
+        const request = new sql.Request();
+        // query to the database and execute procedure
+        request.query("exec dbo.DN_Diag @HN="+hn,  (err, recordset)=> {
+            if (err) console.log(err);
+
+            res.send(recordset);
+        });
+    });
+    sql.close()
+});
+/*
+app.post('/login',function(req,res){
+  var user_name=req.body.user;
+  var password=req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
+});
+*/
+
 
 
 
