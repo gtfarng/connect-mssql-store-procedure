093b5ab959828d029233deb499bc7fe608d4b70a
gtfarng <5735512002@psu.ac.th>
2019-09-26 16:52:32 +0700

    update

--- query/index.js
+++ query/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const app = express()
-const router = express.Router();
-const cors = require('cors')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const sql = require("mssql");
 
 const config = require('./config.js');
 
+const app = express()
+const router = express.Router();
+
 app.use(cors())
 app.use('/api', bodyParser.json(), router)
-app.use('/api', bodyParser.urlencoded({ extended: false }), router)
+app.use('/api', bodyParser.urlencoded({ extended: true }), router)
 
 app.get('/api', (req, res)=>{    res.send(`<html><h1>Welcome To APIs - DoctorNote</h1></html>`)    });
  
@@ -31,40 +32,44 @@ app.get('/api/DoctorListDetails',  (req, res) =>{
 
             // send data as a response
             res.send(recordset);
-           
+            sql.close()
 
         });
 
     });
-     sql.close()
+     
 });
 
 
 app.post('/api/test',  (req, res)=> {
-    const hn = req.body.HN;
+    var hn = req.body.hn;
+   
    
     sql.connect(config,  (err) =>{
 
         if (err) console.log(err);
         // create Request object
-        const request = new sql.Request();
+        let request = new sql.Request();
+    
         // query to the database and execute procedure
-        request.query("exec dbo.DN_Diag @HN="+hn,  (err, recordset)=> {
+        request.query("exec dbo.DN_Diag @HN='"+hn+"'",  (err, recordset)=> {
+            console.log('.POST :/api/Test')
             if (err) console.log(err);
 
             res.send(recordset);
+            sql.close()
         });
     });
-    sql.close()
+    
 });
-/*
-app.post('/login',function(req,res){
+
+app.post('/api/login',function(req,res){
   var user_name=req.body.user;
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
 });
-*/
+
 
 
 
